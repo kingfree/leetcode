ec5619863bb52dbbc9ee213367a89b07c89a8517
Kingfree <kingfree@toyama.moe>
2019-08-01 19:24:14 +0800

    two_sum

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() - 1 {
+            for j in i + 1..nums.len() {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
+            }
+        }
+        vec![0, 1]
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(Solution::two_sum(vec![1, 4, 2, 7, 11, 15], 9), vec![2, 3]);
+}
